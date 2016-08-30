fce4180306919cc624454cd8c65d48d08d6cf76a
Sylphias <sylpharyon@gmail.com>
2016-08-30 13:29:10 +0800

    Quick fix with cursor:pointer

--- js/script.js
+++ js/script.js
@@ -9,8 +9,7 @@ $(document).ready(function() {
   $("input[type='text']").on("tap", function (e) {
    e.preventDefault();
    $(this).focus();
-   $(this).val('')
-   console.log("lol")
+   $(this).get(0).setSelectionRange(0, 9999);
   });
   $('.cash-formatting').autoNumeric('init',{aSign:'$ ',aDec:'.',aSep:',',wEmpty:'zero'});
   $('.decimal-formatting').autoNumeric('init',{aSign:'',aDec:'.',aSep:',',wEmpty:'zero'});
